df6cd77797411099007ab46270fe3e48491b67b1
Cindy <44718422+Cindy1140303@users.noreply.github.com>
2025-09-26 10:01:01 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-    ? '/personalpractice/' // 部署到 GitHub Pages 的子路徑
-    : '/',
-  plugins: [vue()]
-})
+
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/personalpractice/' : '/',
+  plugins: [vue()],
+}))
